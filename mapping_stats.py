25dc99a79e6fcc60a818e2d6ac9e3dca00f75c17
mrmilhaven <32551319+mrmilhaven@users.noreply.github.com>
2024-11-26 16:40:12 -0700

    Uploaded updated mapping_stats.py

--- mapping_stats.py
+++ mapping_stats.py
@@ -12,25 +12,41 @@ output_file=open(input_file.replace(".bam","")+".mapstats.txt",'w')
 num_mapped_only_to_right_chrom=0
 num_mapped_perfectly=0
 num_mapped_to_wrong_chrom=0
-total=0.0
-for read,gread in zip(inbam,gbam):
-    print(str(read.query_name),str(read.reference_name),str(read.reference_start))
-    if read.query_name == gread.query_name and read.is_read1 == gread.is_read1:
+inbam_total = 0
+reverse = 0
+gbam_reads = {}
+inbam_reads = {}
 
-        if read.reference_name == gread.reference_name: 
-            if read.reference_start == gread.reference_start:
-                num_mapped_perfectly+=1
-                print("mapped_perfectly")
+for gread in gbam:
+    gbam_reads[gread.query_name] = [gread.reference_name,gread.reference_start,gread.is_read1,gread.query_sequence]
+    
+golden_depth = len(gbam_reads.keys())
+number_perf_mapped=0
+for read in inbam:
+    if read.query_name in gbam_reads.keys():
+        num_perf_mapped_per_gread=0
+        gread = gbam_reads[read.query_name]
+
+        if read.query_sequence == gread[3] and read.is_read1 == gread[2]:
+
+            if read.reference_name == gread[0]:
+                    
+                if read.reference_start == gread[1]:
+                    num_mapped_perfectly+=1
+                else:
+                    num_mapped_only_to_right_chrom+=1
             else:
-                num_mapped_only_to_right_chrom+=1
-                print("mapped only to right chromosome")
-        else:
-            num_mapped_to_wrong_chrom+=1
-            print("mapped to wrong chromosome")
-    else:
-        input("missing from data")
-    total+=1
-output_file.write("Mapped Perfectly:"+str(num_mapped_perfectly)+'\t'+str(num_mapped_perfectly/total)+'\n')
-output_file.write("Mapped only to right chromosome:"+str(num_mapped_only_to_right_chrom)+'\t'+str(num_mapped_only_to_right_chrom/total)+'\n')
-output_file.write("Mismapped:"+str(num_mapped_to_wrong_chrom)+'\t'+str(num_mapped_to_wrong_chrom/total)+'\n')
-output_file.write("Total:"+str(total))
+                print(read.reference_name+" "+gread[0])
+            inbam_total+=1
+
+print("Number of golden reads:"+str(golden_depth))
+print("Number of real reads:"+str(inbam_total))
+print("Perfectly Mapped:"+str(num_mapped_perfectly))
+print("mapped to wrong chromosome:"+str(num_mapped_to_wrong_chrom))
+print("mapped only to right chromosome:"+str(num_mapped_only_to_right_chrom))
+
+if inbam_total == 0:
+    percentage = "N/A"
+else:
+    percentage = num_mapped_perfectly/golden_depth
+print(str(golden_depth)+','+str(num_mapped_perfectly)+','+str(percentage))
